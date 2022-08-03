68c4c3bcad64e109604713f4ebeea5a3d399020f
Thodery82 <110571007+Thodery82@users.noreply.github.com>
2022-08-03 19:25:43 -0300

    Update main.js

--- Idling/js/main.js
+++ Idling/js/main.js
@@ -1,6 +1,10 @@
 var money = new Decimal(0)
 var mps = new Decimal(0)
 
+document.getElementById("click").onclick = () => {
+  money = money.add(1)
+}
+
 function UpdateGameArea() {
   document.getElementById("money").innerText = money
   document.getElementById("resource").innerText = 'Money'
